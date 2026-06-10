c36d06214bdca6828478ce871492bf2d5b401aab
PRIYANKA-Nigam <nigampriyanka042.com>
2026-06-10 15:42:13 +0530

    CORS 5th Test commit

--- app/api/generateQuiz/route.ts
+++ app/api/generateQuiz/route.ts
@@ -17,7 +17,8 @@ export async function POST(request: Request) {
 
   const body = await request.json();
 
-  const article = body.article;
+  const article = body.article; 
+  console.log(process.env.GEMINI_API_KEY);
 
   const response = await fetch(
     "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" +
